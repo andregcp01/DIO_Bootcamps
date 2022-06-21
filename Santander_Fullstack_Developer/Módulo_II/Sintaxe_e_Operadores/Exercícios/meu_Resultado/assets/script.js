6b974afdd3d21413e600e3bf89fd2007d262c884
andregcp01 <andregcp01@gmail.com>
2022-06-21 01:44:35 -0300

    resolvido parcialmente o exercicio da calculadora xD

---
+++ Santander_Fullstack_Developer/Módulo_II/Sintaxe_e_Operadores/Exercícios/meu_Resultado/assets/script.js
@@ -0,0 +1,27 @@
+function add(){
+	var a = document.getElementById("t1").value;
+	var b = document.getElementById("t2").value;
+	var c = (parseInt(a)+parseInt(b));
+	var d = 10;
+	var e = 20;
+	//alert(parseInt(a)+parseInt(b));
+	//https://www.youtube.com/watch?v=Ua2WsiycWr4 (link do vídeo do camarada que me ajudou xD)
+	if((a)==(b)){
+		alert("A e B são iguais!");
+	}else {
+		alert("A e B não são iguais");
+	}
+	if((c)<(d)) {
+		alert("A soma de A e B é MENOR do que 10!");
+	}else {if((c)<(e)) {
+			alert("A soma de A e B é MAIOR do que 10 e MENOR do que 20!");
+		}else {
+			alert("A soma de A e B é MAIOR do que 20, consenquentemente, MAIOR do que 10!")
+		}
+	}
+	//if((c)<(e)){
+	//	alert("A soma de A e B é MENOR do que 20!");
+	//}else{
+	//	alert("A soma de A e B é MAIOR do que 20!");
+	//}
+}
