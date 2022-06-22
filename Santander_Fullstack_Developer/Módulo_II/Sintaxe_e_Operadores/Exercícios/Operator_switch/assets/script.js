2c60d130a78f16bb9bd7651a51e8308ec5130cad
andregcp01 <andregcp01@gmail.com>
2022-06-22 09:10:14 -0300

    concluido operator switch

--- Santander_Fullstack_Developer/Módulo_II/Sintaxe_e_Operadores/Exercícios/Operator_switch/assets/script.js
+++ Santander_Fullstack_Developer/Módulo_II/Sintaxe_e_Operadores/Exercícios/Operator_switch/assets/script.js
@@ -4,7 +4,9 @@ function add(){
 	var c = (parseInt(a)+parseInt(b));
 	var d = 10;
 	var e = 20;
+	var n, m;
 	//alert(parseInt(a)+parseInt(b));
+		
 	switch (a) {
 		case (b):
 			alert("A e B são iguais!");
@@ -13,14 +15,48 @@ function add(){
 			alert("A e B são diferentes!");
 			break;
 	}
-	if((c)/d>1){
-		alert("A soma de A + B é maior do que 10!");
+	
+	if((c/d)>1) {
+		n = 2;
 	} else {
-			if((c)/d<1){
-				alert("A soma de A + B é menor do que 10!");
+		if((c/d)<1) {
+			n = 1;
 		} else {
-			alert("A soma de A + B é igual a 10!")
+			n = 0;
 		}
 	}
 	
+	switch (n) {
+		case 1:
+			alert("A soma de A + B é menor do que 10!");
+			break;
+		case 2:
+			alert("A soma de A + B é maior do que 10!");
+			break;
+		default:
+			alert("A soma de A + B é igual a 10!");
+			break;
+	}
+	
+	if((c/e)>1) {
+		m = 2;
+	} else {
+		if((c/e)<1) {
+			m = 1;
+		} else {
+			m = 0;
+		}
+	}
+	
+	switch (m) {
+		case 1:
+			alert("A soma de A + B é menor do que 20!");
+			break;
+		case 2:
+			alert("A soma de A + B é maior do que 20!");
+			break;
+		default:
+			alert("A soma de A + B é igual a 20!");
+			break;
+	}
 }
