7b27c8acdc885470c7dd7d210343466c7004ef93
andregcp01 <andregcp01@gmail.com>
2022-06-21 23:14:00 -0300

    concluido o operator if-else

--- Santander_Fullstack_Developer/Módulo_II/Sintaxe_e_Operadores/Exercícios/Operator_if-else/assets/script.js
+++ Santander_Fullstack_Developer/Módulo_II/Sintaxe_e_Operadores/Exercícios/Operator_if-else/assets/script.js
@@ -6,21 +6,26 @@ function add(){
 	var e = 20;
 	//alert(parseInt(a)+parseInt(b));
 	if((a)==(b)){
-		alert("A e B são iguais!");
-	}else {
-		alert("A e B não são iguais");
+		alert("A e B são iguais!")
+	} else {
+		alert("A e B não são iguais")
 	}
-	if((c)<(d)) {
-		alert("A soma de A e B é MENOR do que 10!");
-	}else {if((c)<(e)) {
-			alert("A soma de A e B é MAIOR do que 10 e MENOR do que 20!");
-		}else {
-			alert("A soma de A e B é MAIOR do que 20, consenquentemente, MAIOR do que 10!")
-		}
+	if((c)<(d)){
+		alert("A + B é menor do que 10!")
+	} else {if((c)>(d)){
+				alert("A + B é maior do que 10!")
+	} 		else {
+				alert("A + B é igual a 10!")
 	}
-	//if((c)<(e)){
-	//	alert("A soma de A e B é MENOR do que 20!");
-	//}else{
-	//	alert("A soma de A e B é MAIOR do que 20!");
-	//}
+		
+	}
+	if((c)<(e)){
+		alert("A + B é menor do que 20!")
+	} else {if((c)>(e)){
+				alert("A + B é maior do que 20!")
+	} 		else {
+				alert("A + B é igual a 20!")
+	}
+		
+	}
 }
