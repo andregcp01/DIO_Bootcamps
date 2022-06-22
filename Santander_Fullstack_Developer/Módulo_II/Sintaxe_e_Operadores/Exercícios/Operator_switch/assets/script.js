a424e982dac55fdea9f2b9775ed27d2a004f7ab6
andregcp01 <andregcp01@gmail.com>
2022-06-21 23:20:56 -0300

    adicionados os arquivos para o projeto operator switch

---
+++ Santander_Fullstack_Developer/Módulo_II/Sintaxe_e_Operadores/Exercícios/Operator_switch/assets/script.js
@@ -0,0 +1,31 @@
+function add(){
+	var a = document.getElementById("t1").value;
+	var b = document.getElementById("t2").value;
+	var c = (parseInt(a)+parseInt(b));
+	var d = 10;
+	var e = 20;
+	//alert(parseInt(a)+parseInt(b));
+	if((a)==(b)){
+		alert("A e B são iguais!")
+	} else {
+		alert("A e B não são iguais")
+	}
+	if((c)<(d)){
+		alert("A + B é menor do que 10!")
+	} else {if((c)>(d)){
+				alert("A + B é maior do que 10!")
+	} 		else {
+				alert("A + B é igual a 10!")
+	}
+		
+	}
+	if((c)<(e)){
+		alert("A + B é menor do que 20!")
+	} else {if((c)>(e)){
+				alert("A + B é maior do que 20!")
+	} 		else {
+				alert("A + B é igual a 20!")
+	}
+		
+	}
+}
