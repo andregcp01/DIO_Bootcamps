39bc38e78d4f2a2f5899ed0b58d4facd6f4acb19
andregcp01 <andregcp01@gmail.com>
2022-06-27 22:31:29 -0300

    finalizado o exercicio, mas ele não agi de forma satisfatória quando não são definidos valores

--- Santander_Fullstack_Developer/Módulo_II/Variáveis_e_Tipos/Exercícios/Namer_Measure/assets/script.js
+++ Santander_Fullstack_Developer/Módulo_II/Variáveis_e_Tipos/Exercícios/Namer_Measure/assets/script.js
@@ -1,14 +1,20 @@
-function namelenght(){
+function namelength(){
 	var a = document.getElementById("name").value;
 	var b = document.getElementById("surname").value;
-	
+	var c = 0;
+
 	let firstName = (a);
 		alert(`Certo! Seja bem vindo ${firstName}!`);
-		
+	
 	let secondName = (b);
 		alert(`Beleza! Seu sobrenome é ${secondName}`);
 	
-	let fullName = firstName + " " + secondName;
+	let fullName = `${firstName} ${secondName}`;
 		alert(`Então seu nome é: ${fullName}`);
-
+	
+	let nameLength = (firstName.length);  
+	alert(`Seu primeiro nome tem: ${nameLength} letras!`)
+	
+	let surnameLength = (secondName.length);
+	alert(`Seu segundo nome tem: ${surnameLength} letras!`)
 }
