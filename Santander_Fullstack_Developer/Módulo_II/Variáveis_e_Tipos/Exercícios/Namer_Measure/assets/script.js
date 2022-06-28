41b09c48d8cc910b1eb6cd72d5c30e98d195cbb0
andregcp01 <andregcp01@gmail.com>
2022-06-27 23:30:17 -0300

    adicionado um rp de leve ao namer measure.. xD

--- Santander_Fullstack_Developer/Módulo_II/Variáveis_e_Tipos/Exercícios/Namer_Measure/assets/script.js
+++ Santander_Fullstack_Developer/Módulo_II/Variáveis_e_Tipos/Exercícios/Namer_Measure/assets/script.js
@@ -2,33 +2,37 @@ function namelength(){
 	var a = document.getElementById("name").value;
 	var b = document.getElementById("surname").value;
 	var x = 0;
-
+	
 	let firstName = (a);
 		if((a)!=x) {
 			alert(`Certo! Seja bem vindo ${firstName}!`);
 		} else {
 			firstName = "João";
-			alert(`Humm, então você é o tal do João..`)
+			alert(`Humm, então você é o tal do João..`);
 		}
-		
+	
 	let secondName = (b);
 		if((b)!=x) {
 			alert(`Beleza! Seu sobrenome é ${secondName}`);
 		} else {
 			secondName = "das Neves";
-			alert(`Ok então, já sei qual deve ser..`)
+			alert(`Ok então, já sei qual deve ser..`);
 		}
 	
 	let fullName = `${firstName} ${secondName}`;
-		alert(`Então seu nome é: ${fullName}`);
+		if((fullName) == "João das Neves") {
+			alert (`Então, seu nome é ${fullName} e VOCÊ NÃO SABE NADA!`);
+		} else {
+			alert(`Então, seu nome é ${fullName}`);
+		}
 	
 	let nameLength = (firstName.length);  
-	alert(`Seu primeiro nome tem: ${nameLength} letras!`)
+	alert(`Seu primeiro nome tem: ${nameLength} letras!`);
 	
 	let surnameLength = (secondName.length);
 		if((secondName)="das Neves") {			
-			alert(`Seu segundo nome tem: 8 letras!`)
+			alert(`Seu segundo nome tem: 8 letras!`);
 		} else {
-			alert(`Seu segundo nome tem: ${surnameLength} letras!`)
+			alert(`Seu segundo nome tem: ${surnameLength} letras!`);
 		}
 }
