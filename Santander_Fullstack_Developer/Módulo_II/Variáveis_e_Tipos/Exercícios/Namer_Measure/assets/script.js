1eae11cd55877f735007c2189c7cbe388e92e760
andregcp01 <andregcp01@gmail.com>
2022-06-27 21:01:25 -0300

    pequenas mudanças no namer measure, ele ainda está bugado.. xD

--- Santander_Fullstack_Developer/Módulo_II/Variáveis_e_Tipos/Exercícios/Namer_Measure/assets/script.js
+++ Santander_Fullstack_Developer/Módulo_II/Variáveis_e_Tipos/Exercícios/Namer_Measure/assets/script.js
@@ -1,5 +1,25 @@
 function namelenght(){
-	var a = document.getElementById("t1")
-	var b = document.getElementById("t2")
+	var a = document.getElementById("name");
+	a.setAttribute("type", "text");
+	var b = document.getElementById("surname");
+	b.setAttribute("type", "text");
+	
+	if a = 0 {
+		firstName = João;
+	} else {
+		firstName = "name";
+	}
+	
+	if b = 0 {
+		secondName = das Neves;
+	} else {
+		secondname = "surname";
+	}
+	
+	let firstName = "(a)";
+		alert(`Certo! Seja bem vindo ${firstName}!`);
+		
+	let secondName = "(b)"
+		alert(`Ok! Seu sobrenome é ${secondName}`);
 	
 }
