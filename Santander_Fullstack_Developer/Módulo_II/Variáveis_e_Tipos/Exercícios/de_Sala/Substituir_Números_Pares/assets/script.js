2c94b7c002b3fef4cdc87e1775347a822d128800
andregcp01 <andregcp01@gmail.com>
2022-06-29 17:58:42 -0300

    finalizado o exercício substituição dos números pares

--- Santander_Fullstack_Developer/Módulo_II/Variáveis_e_Tipos/Exercícios/de_Sala/Substituir_Números_Pares/assets/script.js
+++ Santander_Fullstack_Developer/Módulo_II/Variáveis_e_Tipos/Exercícios/de_Sala/Substituir_Números_Pares/assets/script.js
@@ -1,10 +1,38 @@
-function ripevennumbers(){
+function eveniszero(){
 	var a = document.getElementById("sequence").value;
 	
 	let sequenceNumbers = (a);
 	
-	let splitSequence = (a).split("");
-	//alert(`${splitSequence}`)
+	let splitSequence = sequenceNumbers.split("");
+	alert(`${splitSequence}`)
 	
+	function swap(splitSequence) {
+		var b = [];
+		
+		for (var i=0; i < splitSequence.length; i++) {
+			if (a[i] % 2 == 0) {
+				b.push(splitSequence[i]=0);
+			} else {
+				b.push(splitSequence[i])
+			}
+		}
+	return b;
+	}
 	
+	alert(swap(splitSequence));
+	
+	//if (sei la oq % 2 == 0) {
+	//	return 0;
+	//}
 }
+
+//	function swap(splitSequence) {
+//		var b = [];
+//		
+//		for (var i=0; i < splitSequence.length; i++) {
+//			if (a[i] % 2 === 0) {
+//				b.push(a[i]);
+//			}
+//		}
+//	return b;
+//	}
