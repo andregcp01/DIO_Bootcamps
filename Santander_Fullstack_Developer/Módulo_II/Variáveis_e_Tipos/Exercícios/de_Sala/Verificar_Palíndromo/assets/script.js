57814eacb07d1988fb42fb6dcf50a7d2dc8ac90a
andregcp01 <andregcp01@gmail.com>
2022-06-28 23:09:47 -0300

    concluido o verificar palíndromo universal, créditos ao free code camp xD, mas entendendo o passo a passo, ta safe!

---
+++ Santander_Fullstack_Developer/Módulo_II/Variáveis_e_Tipos/Exercícios/de_Sala/Verificar_Palíndromo/assets/script.js
@@ -0,0 +1,19 @@
+function checkword(){
+	var a = document.getElementById("word").value;
+	
+	let reverseString = (a);
+	var splitString = (a).split("");
+	alert(`${splitString}`); // Divisão dos elementos do Array
+	
+	var reverseArray = splitString.reverse();
+	alert(`${reverseArray}`); // Revertendo a ordem dos elementos do Array
+	
+	var joinArray = reverseArray.join("");
+	alert(`${joinArray}`); // Fusão dos elementos ao contrário
+	
+	if((joinArray)==(a)){
+		alert(`A palavra é um palíndromo!`)
+	} else {
+		alert(`A palavra não é um palíndromo!`)
+	}
+}
