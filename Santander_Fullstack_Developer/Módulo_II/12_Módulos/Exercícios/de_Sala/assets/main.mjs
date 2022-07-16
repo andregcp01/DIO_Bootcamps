a23b6a247eba857d8c1ddf232c6401b25665cb2f
andregcp01 <andregcp01@gmail.com>
2022-07-16 17:55:36 -0300

    finalizado o exercício de modulos

---
+++ Santander_Fullstack_Developer/Módulo_II/12_Módulos/Exercícios/de_Sala/assets/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Stephany", 26));
+console.log(mostraCidade("Stephany", "São Paulo"));
+console.log(mostraHobby("Stephany", "Teaching"));
